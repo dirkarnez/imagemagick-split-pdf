86dbddd334266a6abf0176b7ef242b7f5e9a7782
Dirk Arnez <smalldirkalex@gmail.com>
2026-03-25 16:26:52 +0800

    - add single-page-without-crop/drag-pdf-to-this-icon.cmd

---
+++ single-page-without-crop/drag-pdf-to-this-icon.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+set PATH=^
+%~dp0ImageMagick-7.1.2-18-portable-Q16-HDRI-x64;
+
+set "filename_with_extension=%~nx1"
+set "filename=%~n1"
+
+echo Splitting the pdf "%filename_with_extension%" &&^
+magick.exe -density 300 -colorspace sRGB "%~f1[68]" "%filename%_split_137.jpg" &&^
+echo DONE &&^
+pause
