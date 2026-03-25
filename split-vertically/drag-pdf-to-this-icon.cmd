b6fae30b6fee6df232577fe7d33953655e946bd9
Dirk Arnez <smalldirkalex@gmail.com>
2026-03-25 16:13:11 +0800

    - upload files

---
+++ split-vertically/drag-pdf-to-this-icon.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+set PATH=^
+%~dp0ImageMagick-7.1.2-18-portable-Q16-HDRI-x64;
+
+set "filename_with_extension=%~nx1"
+set "filename=%~n1"
+
+echo Splitting the pdf "%filename_with_extension%" &&^
+magick.exe -density 300 "%~f1[1-68]" -colorspace sRGB -crop 2x1@ +repage -scene 1 "%filename%_split_%%d.jpg" &&^
+echo DONE &&^
+pause
